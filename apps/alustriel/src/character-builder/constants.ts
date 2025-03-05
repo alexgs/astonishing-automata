08e0f3dcfdc48be540991695cb1548e3a5bcf6aa
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-03-05 17:04:39 -0500

    AA-11: Replace underscore separator

--- apps/alustriel/src/character-builder/constants.ts
+++ apps/alustriel/src/character-builder/constants.ts
@@ -7,25 +7,27 @@ export const EVENT_TYPES = {
   STARTED: 'event-types.character-builder.character-creation-started',
 } as const;
 
+// Can't use `.` in step names, but can use it in event names. 🤷🏻
+// https://stately.ai/blog/2024-01-23-state-machines-whats-in-a-name#xstate-v5-and-dotcase
 export const STEPS = {
-  SELECT_SPECIES: 'character-builder_steps_select-species',
-  SELECT_CLASS: 'character-builder_steps_select-class',
-  SELECT_SUBCLASS: 'character-builder_steps_select-subclass',
-  SELECT_CLASS_FEATURES: 'character-builder_steps_select-class-features',
+  SELECT_SPECIES: 'character-builder:steps:select-species',
+  SELECT_CLASS: 'character-builder:steps:select-class',
+  SELECT_SUBCLASS: 'character-builder:steps:select-subclass',
+  SELECT_CLASS_FEATURES: 'character-builder:steps:select-class-features',
   CHOOSE_ABILITY_SCORE_METHOD:
-    'character-builder_steps_choose-ability-score-method',
-  ABILITY_SCORE_STANDARD: 'character-builder_steps_ability-score-standard',
-  ABILITY_SCORE_POINT_BUY: 'character-builder_steps_ability-score-point-buy',
-  ABILITY_SCORE_ROLLED: 'character-builder_steps_ability-score-rolled',
-  SELECT_BACKGROUND: 'character-builder_steps_select-background',
+    'character-builder:steps:choose-ability-score-method',
+  ABILITY_SCORE_STANDARD: 'character-builder:steps:ability-score-standard',
+  ABILITY_SCORE_POINT_BUY: 'character-builder:steps:ability-score-point-buy',
+  ABILITY_SCORE_ROLLED: 'character-builder:steps:ability-score-rolled',
+  SELECT_BACKGROUND: 'character-builder:steps:select-background',
   SELECT_BACKGROUND_FEATURES:
-    'character-builder_steps_select-background-features',
-  CHOOSE_EQUIPMENT_METHOD: 'character-builder_steps_choose-equipment-method',
-  EQUIPMENT_STARTING: 'character-builder_steps_equipment-starting',
-  EQUIPMENT_GOLD_BUY: 'character-builder_steps_equipment-gold-buy',
-  SELECT_SPELLS: 'character-builder_steps_select-spells',
-  FINAL_REVIEW: 'character-builder_steps_final-review',
-  FINISH: 'character-builder_steps_finish',
+    'character-builder:steps:select-background-features',
+  CHOOSE_EQUIPMENT_METHOD: 'character-builder:steps:choose-equipment-method',
+  EQUIPMENT_STARTING: 'character-builder:steps:equipment-starting',
+  EQUIPMENT_GOLD_BUY: 'character-builder:steps:equipment-gold-buy',
+  SELECT_SPELLS: 'character-builder:steps:select-spells',
+  FINAL_REVIEW: 'character-builder:steps:final-review',
+  FINISH: 'character-builder:steps:finish',
 } as const;
 
 export const STREAM_TYPES = {
