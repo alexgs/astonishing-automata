3705f132398b9d81216af009be452f790baad018
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-03-16 16:05:32 -0400

    AA-19: Update module test

--- apps/alustriel/module-tests/state-machine.spec.ts
+++ apps/alustriel/module-tests/state-machine.spec.ts
@@ -10,14 +10,54 @@ import { WinstonModule } from 'nest-winston';
 
 import { EVENT_TYPES } from '../src/character-builder/constants';
 import { EventStoreModule } from '../src/event-store/event-store.module';
+import { EventStoreReadModel } from '../src/event-store/interfaces';
 import { TOKENS } from '../src/provider-tokens';
 import { ActorFactory } from '../src/state-machine/actor.factory';
-import { STEPS } from '../src/state-machine/constants';
+import { INITIAL_STEP, STEPS } from '../src/state-machine/constants';
 import { StateMachineModule } from '../src/state-machine/state-machine.module';
 import { testLog } from '../src/winston-transports';
 
 import { MockKnexService } from './mock-knex-service';
 import { mockPostgresService } from './mock-postgres-service';
+
+const CHARACTER_ID = '3dd8d324-ff14-4210-97e3-07e6cf4d729b';
+
+const PAST_EVENTS: EventStoreReadModel[] = [
+  {
+    id: '01JPG3G7Z11P38SHXWSQJNNRZ0',
+    stream_id: CHARACTER_ID,
+    type: EVENT_TYPES.STEP_CHANGED,
+    data: {
+      characterId: CHARACTER_ID,
+      nextStep: STEPS.SELECT_SPECIES,
+    },
+    created_at: new Date(1742149787617),
+    version: 1,
+  },
+  {
+    id: '01JPG3G90WJDGV7BFN727Z7NT7',
+    stream_id: CHARACTER_ID,
+    type: EVENT_TYPES.SPECIES_SELECTED,
+    data: {
+      characterId: CHARACTER_ID,
+      species: 'elf',
+    },
+    created_at: new Date(1742149788700),
+    version: 2,
+  },
+  {
+    id: '01JPG3G9TSMXYDYY055M4TR7J1',
+    stream_id: CHARACTER_ID,
+    type: EVENT_TYPES.STEP_CHANGED,
+    data: {
+      characterId: CHARACTER_ID,
+      previousStep: STEPS.SELECT_SPECIES,
+      nextStep: STEPS.SELECT_CLASS,
+    },
+    created_at: new Date(1742149789529),
+    version: 3,
+  },
+];
 
 describe('State Machine module', () => {
   let actorFactory: ActorFactory;
@@ -78,38 +118,14 @@ describe('State Machine module', () => {
     });
 
     describe('when there are events in the stream', () => {
-      // TODO Add more events and test the actor's data
       it('creates an actor', async () => {
-        const characterId = 'fe92cd2c-9275-4b01-aef1-ab610ca5967d';
         const tracker = mockKnexService.getTracker();
-        tracker.on.select('events').responseOnce([
-          {
-            id: '1',
-            streamId: characterId,
-            type: EVENT_TYPES.STEP_CHANGED,
-            data: {
-              characterId,
-              previousStep: undefined,
-              nextStep: STEPS.SELECT_SPECIES,
-            },
-            createdAt: new Date(),
-          },
-          {
-            id: '2',
-            streamId: characterId,
-            type: EVENT_TYPES.STEP_CHANGED,
-            data: {
-              characterId,
-              previousStep: STEPS.SELECT_SPECIES,
-              nextStep: STEPS.SELECT_CLASS,
-            },
-            createdAt: new Date(),
-          },
-        ]);
+        tracker.on.select('events').responseOnce(PAST_EVENTS);
 
-        const { actor } = await actorFactory.getActor(characterId);
+        const { actor } = await actorFactory.getActor(CHARACTER_ID);
         expect(actor).toBeDefined();
         expect(actor.getSnapshot().value).toEqual(STEPS.SELECT_CLASS);
+        expect(actor.getSnapshot().context.species).toEqual('elf');
       });
     });
   });
