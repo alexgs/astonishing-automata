6ce1a6a0a40d05e6cda3f862dc48224e15600aaf
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-03-20 18:45:10 -0400

    AA-24: Remove Skeleton UI

--- apps/bruenor/src/routes/+page.svelte
+++ apps/bruenor/src/routes/+page.svelte
@@ -2,12 +2,12 @@
   - Copyright 2025 Phillip Gates-Shannon. All rights reserved. Licensed under the Elastic License 2.0 (ELv2).
   -->
 
-<h1 class="h1">Welcome to Automata Character Builder!</h1>
+<h1>Welcome to Automata Character Builder!</h1>
 <div>
-  <h2 class="h2">Your Characters</h2>
+  <h2>Your Characters</h2>
   <p>Eventually a list of characters will go here.</p>
 </div>
 <div>
-  <h2 class="h2">Start a new character:</h2>
-  <button type="button" class="btn preset-filled-primary-500">Start</button>
+  <h2>Start a new character:</h2>
+  <button type="button">Start</button>
 </div>
