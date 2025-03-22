b26d81d19b322b32ceb14f931d0a667188a656a6
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-03-22 09:11:24 -0400

    AA-26: Add basic Clerk authentication

---
+++ apps/bruenor/src/routes/user/+page.svelte
@@ -0,0 +1,25 @@
+<!--
+  - Copyright 2025 Phillip Gates-Shannon. All rights reserved. Licensed under the Elastic License 2.0 (ELv2).
+  -->
+
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { clerk } from '$lib/clerk';
+  import type { UserResource } from '@clerk/types';
+
+  let user: UserResource | null = null;
+
+  onMount(async () => {
+    if (clerk.user) {
+      user = clerk.user;
+    // } else {
+    //   await clerk.openSignIn();
+    }
+  });
+</script>
+
+{#if user}
+  <p>Welcome, {user.firstName}!</p>
+{:else}
+  <button on:click={() => clerk.openSignIn()}>Sign In</button>
+{/if}
