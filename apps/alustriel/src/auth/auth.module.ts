3d4edfa1fd40a76490383b68dc3ce8ec66472753
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-03-23 12:22:22 -0400

    AA-29: Add copyright statements

--- apps/alustriel/src/auth/auth.module.ts
+++ apps/alustriel/src/auth/auth.module.ts
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2025 Phillip Gates-Shannon. All rights reserved. Licensed under the Elastic License 2.0 (ELv2).
+ */
+
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 
--- apps/alustriel/src/auth/guards/clerk-auth-guard.guard.ts
+++ apps/alustriel/src/auth/guards/clerk-auth-guard.guard.ts
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2025 Phillip Gates-Shannon. All rights reserved. Licensed under the Elastic License 2.0 (ELv2).
+ */
+
 import { getAuth } from '@clerk/express';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
