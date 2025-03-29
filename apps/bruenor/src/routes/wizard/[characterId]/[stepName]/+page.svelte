b843f69cbd2d8d4ee1b17048ffaf8060c1453648
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-03-28 20:46:23 -0400

    AA-32: Create a page for the wizard

---
+++ apps/bruenor/src/routes/wizard/[characterId]/[stepName]/+page.svelte
@@ -0,0 +1,18 @@
+<!--
+  - Copyright 2025 Phillip Gates-Shannon. All rights reserved. Licensed under the Elastic License 2.0 (ELv2).
+  -->
+
+<script>
+  import LayoutGrid, { Cell } from '@smui/layout-grid';
+  import { page } from '$app/state';
+
+  const { characterId, stepName } = page.params;
+
+</script>
+
+<LayoutGrid>
+  <Cell span={12}>
+    <p>Character ID: {characterId}</p>
+    <p>Step name: {stepName}</p>
+  </Cell>
+</LayoutGrid>
--- packages/state-machine/src/constants.ts
+++ packages/state-machine/src/constants.ts
@@ -10,24 +10,24 @@ export const ACTIONS = {
 // Can't use `.` in step names, but can use it in event names. 🤷🏻
 // https://stately.ai/blog/2024-01-23-state-machines-whats-in-a-name#xstate-v5-and-dotcase
 export const STEPS = {
-  SELECT_SPECIES: 'character-builder:steps:select-species',
-  SELECT_CLASS: 'character-builder:steps:select-class',
-  SELECT_SUBCLASS: 'character-builder:steps:select-subclass',
-  SELECT_CLASS_FEATURES: 'character-builder:steps:select-class-features',
+  SELECT_SPECIES: 'character-builder~steps~select-species',
+  SELECT_CLASS: 'character-builder~steps~select-class',
+  SELECT_SUBCLASS: 'character-builder~steps~select-subclass',
+  SELECT_CLASS_FEATURES: 'character-builder~steps~select-class-features',
   CHOOSE_ABILITY_SCORE_METHOD:
-    'character-builder:steps:choose-ability-score-method',
-  ABILITY_SCORE_STANDARD: 'character-builder:steps:ability-score-standard',
-  ABILITY_SCORE_POINT_BUY: 'character-builder:steps:ability-score-point-buy',
-  ABILITY_SCORE_ROLLED: 'character-builder:steps:ability-score-rolled',
-  SELECT_BACKGROUND: 'character-builder:steps:select-background',
+    'character-builder~steps~choose-ability-score-method',
+  ABILITY_SCORE_STANDARD: 'character-builder~steps~ability-score-standard',
+  ABILITY_SCORE_POINT_BUY: 'character-builder~steps~ability-score-point-buy',
+  ABILITY_SCORE_ROLLED: 'character-builder~steps~ability-score-rolled',
+  SELECT_BACKGROUND: 'character-builder~steps~select-background',
   SELECT_BACKGROUND_FEATURES:
-    'character-builder:steps:select-background-features',
-  CHOOSE_EQUIPMENT_METHOD: 'character-builder:steps:choose-equipment-method',
-  EQUIPMENT_STARTING: 'character-builder:steps:equipment-starting',
-  EQUIPMENT_GOLD_BUY: 'character-builder:steps:equipment-gold-buy',
-  SELECT_SPELLS: 'character-builder:steps:select-spells',
-  FINAL_REVIEW: 'character-builder:steps:final-review',
-  FINISH: 'character-builder:steps:finish',
+    'character-builder~steps~select-background-features',
+  CHOOSE_EQUIPMENT_METHOD: 'character-builder~steps~choose-equipment-method',
+  EQUIPMENT_STARTING: 'character-builder~steps~equipment-starting',
+  EQUIPMENT_GOLD_BUY: 'character-builder~steps~equipment-gold-buy',
+  SELECT_SPELLS: 'character-builder~steps~select-spells',
+  FINAL_REVIEW: 'character-builder~steps~final-review',
+  FINISH: 'character-builder~steps~finish',
 } as const;
 
 export const INITIAL_STEP = STEPS.SELECT_SPECIES;
