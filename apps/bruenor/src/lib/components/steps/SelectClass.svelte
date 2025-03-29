47cb1c38e14af347e35c29dff4a33a8ac942e5df
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-03-29 12:44:22 -0400

    AA-32: Add step components

---
+++ apps/bruenor/src/lib/components/steps/SelectClass.svelte
@@ -0,0 +1,29 @@
+<!--
+  - Copyright 2025 Phillip Gates-Shannon. All rights reserved. Licensed under the Elastic License 2.0 (ELv2).
+  -->
+
+<script lang="ts">
+  import { Cell } from '@smui/layout-grid';
+  import type { Readable } from 'svelte/store';
+  import type { AnyMachineSnapshot, EventObject } from 'xstate';
+  import {
+    characterBuilderMachine,
+    getNextSteps,
+  } from '@automata/state-machine';
+
+  interface Props {
+    characterId: string;
+    send: (event: EventObject) => void;
+    snapshot: Readable<AnyMachineSnapshot>;
+  }
+
+  const { characterId, send, snapshot }: Props = $props();
+
+  console.log(`Value: ${$snapshot.value}`);
+  const nextSteps = getNextSteps(characterBuilderMachine, $snapshot, $snapshot.value);
+  console.log(`Next steps: ${JSON.stringify(nextSteps, null, 2)}`);
+</script>
+
+<Cell span={12}>
+  <p>Select class</p>
+</Cell>
--- apps/bruenor/src/routes/wizard/[characterId]/[stepName]/+page.svelte
+++ apps/bruenor/src/routes/wizard/[characterId]/[stepName]/+page.svelte
@@ -2,16 +2,45 @@
   - Copyright 2025 Phillip Gates-Shannon. All rights reserved. Licensed under the Elastic License 2.0 (ELv2).
   -->
 
-<script>
+<script lang="ts">
   import {
+    STEPS,
+    type StepName,
     characterBuilderMachine,
     getNextSteps,
   } from '@automata/state-machine';
-  import LayoutGrid, { Cell } from '@smui/layout-grid';
-  import { page } from '$app/state';
+  import LayoutGrid from '@smui/layout-grid';
+  import { useMachine } from '@xstate/svelte';
   import { initialTransition } from 'xstate';
 
+  import { page } from '$app/state';
+  import SelectClass from '$lib/components/steps/SelectClass.svelte';
+  import SelectSpecies from '$lib/components/steps/SelectSpecies.svelte';
+
   const { characterId, stepName } = page.params;
+
+  const { snapshot, send } = useMachine(characterBuilderMachine);
+
+  const stepComponentMap = {
+    [STEPS.SELECT_SPECIES]: SelectSpecies,
+    [STEPS.SELECT_CLASS]: SelectClass,
+    [STEPS.SELECT_SUBCLASS]: SelectClass,
+    [STEPS.SELECT_CLASS_FEATURES]: SelectClass,
+    [STEPS.CHOOSE_ABILITY_SCORE_METHOD]: SelectClass,
+    [STEPS.ABILITY_SCORE_STANDARD]: SelectClass,
+    [STEPS.ABILITY_SCORE_POINT_BUY]: SelectClass,
+    [STEPS.ABILITY_SCORE_ROLLED]: SelectClass,
+    [STEPS.SELECT_BACKGROUND]: SelectClass,
+    [STEPS.SELECT_BACKGROUND_FEATURES]: SelectClass,
+    [STEPS.CHOOSE_EQUIPMENT_METHOD]: SelectClass,
+    [STEPS.EQUIPMENT_STARTING]: SelectClass,
+    [STEPS.EQUIPMENT_GOLD_BUY]: SelectClass,
+    [STEPS.SELECT_SPELLS]: SelectClass,
+    [STEPS.FINAL_REVIEW]: SelectClass,
+    [STEPS.FINISH]: SelectClass,
+  };
+
+  const StepComponent = stepComponentMap[stepName as StepName];
 
   // TODO Eventually we'll need to use a different state than `initialState`
   const [initialState] = initialTransition(characterBuilderMachine);
@@ -20,8 +49,9 @@
 </script>
 
 <LayoutGrid>
-  <Cell span={12}>
-    <p>Character ID: {characterId}</p>
-    <p>Step name: {stepName}</p>
-  </Cell>
+  {#if StepComponent}
+    <svelte:component this={StepComponent} snapshot={snapshot} {send} {characterId} />
+  {:else}
+    <p>Step not found: {stepName}</p>
+  {/if}
 </LayoutGrid>
