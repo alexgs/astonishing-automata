19ccc68db6bef11c95b280f5333587fec700c8ae
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-06-22 13:16:24 -0400

    AA-44: Add types for game system definitions

---
+++ apps/chenowyn/src/lib/grimoire/types.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025 Phillip Gates-Shannon. All rights reserved. Licensed under the Elastic License 2.0 (ELv2).
+ */
+
+export type CharacterSchema = {
+  [key: string]: FieldDefinition | GroupFieldDefinition;
+};
+
+export type Constraint = {
+  if: { [field: string]: { $eq?: unknown; $lt?: unknown; $in?: unknown; } };
+  then: { allowed: boolean; reason?: string };
+};
+
+export type FieldDefinition = {
+  key: string;
+  type: 'string' | 'number' | 'choice' | 'boolean';
+  required?: boolean;
+  options?: string[];
+  constraints?: Constraint[];
+};
+
+export type GameSystemDefinition = {
+  id: string;
+  name: string;
+  character: CharacterSchema;
+  steps: StepDefinition[];
+};
+
+export type GroupFieldDefinition = {
+  type: 'group';
+  fields: { [key: string]: FieldDefinition };
+};
+
+export type StepDefinition = {
+  key: string;
+  fields: string[]; // List of dot-separated field paths, e.g. 'attributes.agility'
+};
