d1127475e74f7661d297d380c57d263e8aad3aca
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-06-27 08:29:22 -0400

    AA-46: Reformat file

--- apps/chenowyn/src/routes/step-explorer/+page.svelte
+++ apps/chenowyn/src/routes/step-explorer/+page.svelte
@@ -5,9 +5,12 @@
 <script lang="ts">
   import { writable, derived } from 'svelte/store';
 
-  import { SavageWorlds } from '@automata/grimoire';
+  import {
+    type FieldDefinition,
+    type GroupFieldDefinition,
+    SavageWorlds,
+  } from '@automata/grimoire';
   import Field from '$lib/components/Field.svelte';
-  import type { FieldDefinition, GroupFieldDefinition } from '@automata/grimoire';
 
   const definition = SavageWorlds;
   const stepIndex = writable(0);
@@ -52,12 +55,12 @@
 <main class="p-4 space-y-4">
   <h1 class="text-2xl font-bold">{definition.name}</h1>
 
-    <h2>Step: {$currentStep.key}</h2>
-    <form>
-      {#each $currentStep.fields as path (path)}
-        <Field {characterState} fieldDef={getFieldDefFromPath(path)} {path} />
-      {/each}
-    </form>
+  <h2>Step: {$currentStep.key}</h2>
+  <form>
+    {#each $currentStep.fields as path (path)}
+      <Field {characterState} fieldDef={getFieldDefFromPath(path)} {path} />
+    {/each}
+  </form>
 
   <div class="flex gap-4">
     <button onclick={prev} class="bg-gray-200 rounded px-4 py-2">Previous</button>
