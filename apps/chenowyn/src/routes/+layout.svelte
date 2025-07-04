b29a8f7b4d49584250a75197b1c2e9814c7319dc
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-07-04 14:54:43 -0400

    AA-48: Add auth to `chenowyn`

---
+++ apps/chenowyn/src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  import { auth } from '$lib/clerk';
+  import AppContainer from '$lib/components/AppContainer.svelte';
+  import NavBar from '$lib/components/NavBar.svelte';
+  import '../app.css';
+
+  auth.provideStore(auth.userStore);
+
+  onMount(() => {
+    auth.init();
+  });
+
+	let { children } = $props();
+</script>
+
+<style>
+  :global(body) {
+    background: transparent;
+  }
+
+  /*noinspection CssReplaceWithShorthandSafely*/
+  .gradient {
+    background: linear-gradient(to bottom, transparent, var(--purple-3));
+    background-color: #212121;
+    height: 50vh;
+    width: 100vw;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+  }
+</style>
+
+<div class="gradient"></div>
+
+<NavBar />
+
+<AppContainer>
+  <main style="margin-top: 64px">
+    {@render children()}
+  </main>
+</AppContainer>
