82ab39101ae58325651bd5d762f39bf145d83eb2
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-07-06 13:08:54 -0400

    AA-53: Dynamically generate form

--- apps/chenowyn/src/lib/components/wizard/DynamicStep.svelte
+++ apps/chenowyn/src/lib/components/wizard/DynamicStep.svelte
@@ -4,19 +4,56 @@
 
 <script lang="ts">
   import { Cell } from '@smui/layout-grid';
-  import { SavageWorlds } from '@automata/grimoire';
+  import {
+    type FieldDefinition,
+    type GroupFieldDefinition,
+    SavageWorlds,
+  } from '@automata/grimoire';
+  import Field from '$lib/components/Field.svelte';
+  import { writable } from 'svelte/store';
 
   interface Props {
     characterId: string;
     step: string;
   }
 
-  const { characterId, step }: Props = $props()
-  const currentStep = SavageWorlds.steps.find(s => s.key === step) ?? { key: 'Unknown' };
+  const { characterId, step }: Props = $props();
+
+  // Create character state with default values
+  const characterState = writable<Record<string, unknown>>({});
+
+  // We change steps by reloading this component, so this should be fine
+  const currentStep = SavageWorlds.steps.find(s => s.key === step) ?? { key: 'Unknown', fields: [] };
+
+  function getFieldDefFromPath(path: string): FieldDefinition {
+    const segments = path.split('.');
+    let node = SavageWorlds.character[segments[0]];
+    if (!node) {
+      throw new Error(`Field definition not found for path: ${path}`);
+    }
+
+    for (let i = 1; i < segments.length; i++) {
+      const segment = segments[i];
+      if (!node) {
+        throw new Error(`Field definition not found for path: ${path}`);
+      }
+
+      if (node.type === 'group') {
+        const group = node as GroupFieldDefinition;
+        node = group.fields?.[segment];
+      }
+    }
+
+    return node as FieldDefinition;
+  }
+
 </script>
 
 <Cell span={12}>
   <h2>Step: {currentStep.key}</h2>
-  <p>Character ID: {characterId}</p>
-  <p>👋 Hello step {step}</p>
+  <form>
+    {#each currentStep.fields as path (path)}
+      <Field {characterState} fieldDef={getFieldDefFromPath(path)} {path} />
+    {/each}
+  </form>
 </Cell>
