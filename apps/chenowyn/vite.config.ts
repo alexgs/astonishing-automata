e408f6bff4529d854b97ff152460d35826386299
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-07-11 08:09:12 -0400

    AA54: Get unit tests running

--- apps/chenowyn/vite.config.ts
+++ apps/chenowyn/vite.config.ts
@@ -2,12 +2,13 @@
  * Copyright 2025 Phillip Gates-Shannon. All rights reserved. Licensed under the Elastic License 2.0 (ELv2).
  */
 
-import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svelteTesting } from '@testing-library/svelte/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()],
+  plugins: [sveltekit(), devtoolsJson()],
   build: {
     target: 'es2022'
   },
@@ -15,32 +16,43 @@ export default defineConfig({
     allowedHosts: true,
   },
 
-	test: {
-		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'client',
-					environment: 'browser',
-					browser: {
-						enabled: true,
-						provider: 'playwright',
-						instances: [{ browser: 'chromium' }]
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
-	}
+  test: {
+    projects: [
+      {
+        extends: './vite.config.ts',
+        plugins: [svelteTesting()],
+
+        test: {
+          name: 'unit',
+          environment: 'jsdom',
+          globals: true,
+          include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+          exclude: ['src/lib/server/**'],
+          setupFiles: ['./vitest-setup-unit.ts'], // optional
+        },
+      },
+      {
+        test: {
+          name: 'e2e',
+          environment: 'browser',
+          browser: {
+            enabled: true,
+            provider: 'playwright',
+            instances: [{ browser: 'chromium' }]
+          },
+          include: ['e2e/**/*.{test,spec}.{js,ts}'],
+          exclude: ['src/**'],
+          setupFiles: ['./vitest-setup-e2e.ts']
+        }
+      },
+      {
+        test: {
+          name: 'logic',
+          environment: 'node',
+          include: ['src/**/*.{test,spec}.{js,ts}'],
+          exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+        }
+      }
+    ]
+  }
 });
